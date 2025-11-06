807f32778a6a326230f044f694877eb60089d559
MedinaMamedova <medinam.mamedovamm@gmail.com>
2025-11-06 12:39:53 +0400

    new task

---
+++ eolymp 923.py
@@ -0,0 +1,13 @@
+a= int(input())
+Summer = [6,7,8]
+Spring = [3,4,5]
+Autumn = [9,10,11]
+Winter = [12,1,2]
+if a in Summer:
+    print("Summer")
+elif a in Spring:
+    print("Spring")
+elif a in Autumn:
+    print("Autumn")
+else:
+    print("Winter")
